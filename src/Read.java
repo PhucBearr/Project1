5faab8a251f7afcc3c4502db19e836456ac466d2
Phuc <Phuc@PHUC-PC>
2020-10-09 18:33:19 +0700

    Update

--- src/Read.java
+++ src/Read.java
@@ -3,20 +3,14 @@ import java.util.Scanner;
 
 public class Read {
     public static void main(String [] args) throws IOException {
-        //Read
-        FileReader reader = new FileReader("C:\\Something.txt");
-        BufferedReader bufferedReader = new BufferedReader(reader);
-        String content =  bufferedReader.readLine();
-        System.out.println(content);
-        bufferedReader.close();
-        //Write
-        FileWriter writer = new FileWriter("D:\\Something2.txt");
-        BufferedWriter bufferedWriter = new BufferedWriter(writer);
         Scanner in = new Scanner(System.in);
-        System.out.println("Write anything");
-        String s = in.nextLine();
-        bufferedWriter.write(s);
-        bufferedWriter.close();
-        System.out.println("Done");
+        System.out.println("Enter path");
+        String file = in.nextLine();
+       File inputFile = new File(file);
+       Scanner IFile = new Scanner(inputFile);
+        while(IFile.hasNext()){
+            System.out.println(IFile.nextLine());
+
+        }
     }
 }
